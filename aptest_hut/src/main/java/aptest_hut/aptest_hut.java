09791401bfa2ba10199b59fb8bae9215921f4d72
garycho <gary.chuo@gmail.com>
2017-02-15 16:06:36 +0800

    123123

--- aptest_hut/src/main/java/aptest_hut/aptest_hut.java
+++ aptest_hut/src/main/java/aptest_hut/aptest_hut.java
@@ -4,7 +4,7 @@ public class aptest_hut {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GitHub Ya Ya 123");
+		System.out.println("GitHub Ya Ya 123 123");
 	}
 
 }
